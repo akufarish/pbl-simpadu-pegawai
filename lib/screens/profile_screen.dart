4f8a76321f95a644d22ca73fa47158d2a78da382
akufarish <risfarishk@gmail.com>
2026-06-01 13:36:38 +0800

    profile done

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:dice_bear/dice_bear.dart';
+import 'package:intl/intl.dart';
 import 'package:pegawai/models/user.dart';
 import 'package:pegawai/providers/user_provider.dart';
 import 'package:pegawai/utils/app_colors.dart';
@@ -27,9 +28,10 @@ class _ProfileScreenState extends State<ProfileScreen> {
   Widget build(BuildContext context) {
     final UserProvider userProvider = context.watch<UserProvider>();
     final UserResponse? user = userProvider.data;
+    final dataPegawai = userProvider.dataPegawai;
 
     return Scaffold(
-      body: userProvider.isLoading
+      body: userProvider.isLoading || user == null || dataPegawai == null
           ? Center(
               child: CircularProgressIndicator(color: AppColors.primaryColor),
             )
@@ -55,6 +57,111 @@ class _ProfileScreenState extends State<ProfileScreen> {
                     child: _profileCard(
                       userProvider.data!.name,
                       userProvider.dataPegawai?.nik ?? "joy",
+                    ),
+                  ),
+                ),
+                SliverPadding(
+                  padding: const EdgeInsets.only(top: 18, left: 23, right: 23),
+                  sliver: SliverToBoxAdapter(
+                    child: Card(
+                      color: Colors.white,
+                      elevation: 3,
+                      child: DefaultTabController(
+                        length: 3,
+                        child: Column(
+                          children: [
+                            TabBar(
+                              tabAlignment: TabAlignment.start,
+                              isScrollable: true,
+                              labelColor: AppColors.primaryColor,
+                              unselectedLabelColor: Colors.grey,
+                              tabs: [
+                                Tab(text: "Informasi Umum"),
+                                Tab(text: "Informasi akun"),
+                                Tab(text: "Domisili"),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 700,
+                              child: TabBarView(
+                                children: [
+                                  // informasi umum
+                                  _buildTabContent([
+                                    CardInfo(
+                                      dataPegawai!.employeeName,
+                                      "Nama:",
+                                    ),
+                                    const SizedBox(height: 12),
+                                    const Divider(),
+                                    const SizedBox(height: 12),
+                                    CardInfo(dataPegawai.nik, "NIK:"),
+                                    const Divider(),
+                                    const SizedBox(height: 12),
+                                    CardInfo(dataPegawai.nip, "NIP:"),
+                                    const Divider(),
+                                    const SizedBox(height: 12),
+                                    CardInfo(dataPegawai.gender!, "Gender:"),
+                                    const Divider(),
+                                    const SizedBox(height: 12),
+                                    CardInfo(
+                                      dataPegawai.phoneNumber?.toString() ??
+                                          "Harap diisi",
+                                      "Phone Number:",
+                                    ),
+                                    const Divider(),
+                                    const SizedBox(height: 12),
+                                    CardInfo(
+                                      DateFormat("EEEE, MMMM d, yyyy").format(
+                                        DateTime.parse(
+                                          dataPegawai.birthDate ??
+                                              DateTime.now().toString(),
+                                        ),
+                                      ),
+                                      "Tanggal Lahir:",
+                                    ),
+                                  ]),
+                                  // Informasi akun
+                                  _buildTabContent([
+                                    CardInfo(
+                                      userProvider.data!.email,
+                                      "Email:",
+                                    ),
+                                    const Divider(),
+                                    const SizedBox(height: 12),
+                                    CardInfo(
+                                      userProvider.data!.roleName,
+                                      "Role:",
+                                    ),
+                                    const Divider(),
+                                    const SizedBox(height: 12),
+                                  ]),
+                                  // Domisili
+                                  _buildTabContent([
+                                    CardInfo(
+                                      dataPegawai.city?.name ?? "Harap diisi",
+                                      "Kota:",
+                                    ),
+                                    const Divider(),
+                                    const SizedBox(height: 12),
+                                    CardInfo(
+                                      dataPegawai.province?.name ??
+                                          "Harap diisi",
+                                      "Provinsi:",
+                                    ),
+                                    const Divider(),
+                                    const SizedBox(height: 12),
+                                    CardInfo(
+                                      dataPegawai.province?.name ??
+                                          "Harap diisi",
+                                      "Provinsi:",
+                                    ),
+                                  ]),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
                   ),
                 ),
@@ -102,5 +209,26 @@ Card _profileCard(String nama, String email) {
         ],
       ),
     ),
+  );
+}
+
+Widget _buildTabContent(List<Widget> children) {
+  return SingleChildScrollView(
+    padding: const EdgeInsets.all(15.0),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: children,
+    ),
+  );
+}
+
+Column CardInfo(String data, String label) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Text(label, style: TextStyle(fontSize: 20, color: Colors.grey)),
+      SizedBox(height: 12),
+      Text(data, style: TextStyle(fontSize: 20)),
+    ],
   );
 }
